17075a584a57031d82075565e7f6f316fe214577
潮汐名利 <113405668+lxloveym@users.noreply.github.com>
2023-08-04 14:01:25 +0800

    Update database.php

--- book/book/application/database.php
+++ book/book/application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname' => '127.0.0.1',
     // 数据库名
-    'database' => 'zuopin',
+    'database' => '',
     // 用户名
-    'username' => 'zuopin',
+    'username' => '',
     // 密码
-    'password' => 'RGRMNBeEtnCj8kdb',
+    'password' => '',
     // 端口
     'hostport' => '3306',
     // 连接dsn
